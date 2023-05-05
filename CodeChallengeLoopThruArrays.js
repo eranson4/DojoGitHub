af4b56753fa6cfeb17ce5654ebabaca13756dbcb
eranson4 <edwardranson87@gmail.com>
2023-05-05 12:53:08 -0700

    LoopThruArrayDojo

---
+++ CodeChallengeLoopThruArrays.js
@@ -0,0 +1,34 @@
+// Challenge 1
+
+// var arr = ["Up", "You", "Give", "To", "Going", "Never"];
+// for(var i = arr.length-1; i >= 0; i = i - 1) { //arr.length starts at "Never" i -1 means it is going down right to left.
+//     console.log(arr[i]);
+// }
+// Prediction number 1: Never, Going, To, Give, You, Up
+
+
+
+
+// Challenge 2
+
+// var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// var sum = 0;
+// for(var i = 1; i < arr.length; i = i + 2) {
+//     sum = sum + arr[i];// remember that i is the position in index [i]; i=3 is index[4] in the array 
+//     console.log(arr[i]);
+//     console.log(sum);
+// }
+// Prediction number 2: 2,2,4,6,6,12,8,20,10,30
+
+
+
+
+// Challenge 3
+
+// var arr = [8, 2, 0, 6, 12, 4, 3];
+// for(var i = 0; i < arr.length; i++){
+//     if(arr[i] >= 6) {  
+//         console.log(true);
+//     }
+// }
+// Prediction number 3: true, true, true
