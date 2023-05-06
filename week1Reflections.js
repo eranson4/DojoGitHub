3c13f640350ef20987c5ea6c662240588a677354
eranson4 <edwardranson87@gmail.com>
2023-05-06 09:16:25 -0700

    week1Reflections

---
+++ week1Reflections.js
@@ -0,0 +1,38 @@
+/*                   Week One Reflections Assignment
+
+First, answer: how have you noticed your bootcamp knowledge grow 
+or change during Programming Basics?
+
+Answer 1: I would say that my knowledge is definitly improving from
+the Algo app practice and code practice. I would say that having some mental
+clicks while doing a few problems on a T-diagram really made some growth in
+the way I can now solve a few algorithms.
+
+
+Then, answer: how have you noticed your approach to learning change 
+throughout Programming Basics?
+
+Answer 2: My approach has changed as far as just trying to stay positive
+and keep working on problems even if I dont exactly understand whats going on,
+and sometimes I have a good breakthrough.
+
+
+
+Finally, share: what do you consider your "Mistake of the Week," 
+which was a valuable failure you learned from during Programming Basics?
+
+Answer 3: I had many mistakes of the week, but I would say my biggest one
+was under utilizing the T-Diagram in the start. I am still very green on
+how to really use a T-Diagram. But if I would have figured out a bit sooner,
+I definitly feel I could have a better understanding on how to break down code 
+line by line. I ended up wasting about 8 hours the first week not really knowing
+how to utilize a T-Diagram. I tried to use it on the computer and hand made them
+on note pad, then in microsoft paint, then in google docs, and finally I just
+went to paper after wasting so much valuable and crucial learning time. 
+Also not really grasping the coding concepts and having to delete code I 
+spent a few hours on building and over complicating code that should have been
+built more simply. What I learned is to just try and simplify things and get
+it to work to keep up in the classes. Also I will just keep pushing forward and
+trying to learn as much as I can because I truly do enjoy solving problems
+and having break throughs!
+*/
