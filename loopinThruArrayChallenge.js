18721684c2d0d7acd8976026fb5e87723d89c174
eranson4 <edwardranson87@gmail.com>
2023-05-06 08:23:57 -0700

    DojoLoopArrChalng

---
+++ loopinThruArrayChallenge.js
@@ -0,0 +1,46 @@
+// #1 write a for loop that will traverse through an array of numbers
+// and print each number     var arr1 = [8, 6, 7, 5, 3, 0, 9];
+
+// function allNums(arr1){
+//     for (let i = 0; i < arr.length; i++) {       
+//     }
+//     console.log(arr1)
+//     return arr;
+// }
+// allNums([8, 6, 7, 5, 3, 0, 9])
+
+
+//2. Write a for loop that will traverse through an array of numbers,
+// and print the sum of the number and the index of the number in the array.
+
+
+// function sumNumIndex(arr2){
+
+// for(var i = 0; i < arr2.length; i++){
+//     console.log(i + arr2[i])
+// }
+// }
+// sumNumIndex([4, 7, 13, 13, 19, 37, -2]);
+
+
+
+//Write a for loop that will traverse through an array of numbers, 
+// and print only the numbers greater than 5.
+
+// var arr3 = [6, 2, 12, 14, -24, 5, 0];
+// for(var i = 0; i <= 7; i++){
+//     if(arr3[i] > 5){
+//         console.log(arr3[i]);
+//     }
+// }
+
+//Ninja Bonus: Modify your solution for #3 so that any numbers in the 
+// array that are not greater than 5 are instead replaced with a string of
+// "No dice." This string should not be printed.
+
+// var arr3 = [6, "No dice", 12, 14, "No dice", "No dice", "No dice"];
+// for(var i = 0; i <= 7; i++){
+//     if(arr3[i] > 5){
+//         console.log(arr3[i]);
+//     }
+// }
