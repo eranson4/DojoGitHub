44b4aadf849d03a8e5c3759e4f77e375291131dd
eranson4 <edwardranson87@gmail.com>
2023-05-06 19:09:59 -0700

    PortfolioContent

--- loopinThruArrayChallenge.js
+++ loopinThruArrayChallenge.js
@@ -28,7 +28,7 @@
 // and print only the numbers greater than 5.
 
 // var arr3 = [6, 2, 12, 14, -24, 5, 0];
-// for(var i = 0; i <= 7; i++){
+// for(var i = 0; i arr3.length 7; i++){
 //     if(arr3[i] > 5){
 //         console.log(arr3[i]);
 //     }
@@ -39,8 +39,29 @@
 // "No dice." This string should not be printed.
 
 // var arr3 = [6, "No dice", 12, 14, "No dice", "No dice", "No dice"];
-// for(var i = 0; i <= 7; i++){
+// for(var i = 0; i <= arr3.length; i++){
 //     if(arr3[i] > 5){
 //         console.log(arr3[i]);
 //     }
 // }
+
+// Aarons Bonus Challenge : same problem, but I want the function
+// to return the result, not print to console, the new values in an array
+
+// function sumNumIndex(arr){
+//     // declaring an empty array to push the results into
+//     var result = [];
+//     // For loop to go through the array based on the array length
+//     for (var i = 0; i < arr.length; i++){
+//         // Console log the sum of the number in the array and the index of the number in the array.
+//         // console.log(arr[i] + i); // commenting this out because it only verifies what we're pushing into the array
+//         result.push(arr[i] + i);
+//     }
+//     return result;
+// }
+
+// var testArray1 = [12, 5, 77, -3, -52, 0];
+// var testArray2 = [5, 4, 3, 2, 1];
+
+// console.log(sumNumIndex(testArray1)) // result should be [12, 6, 79, 0, -48, 5]
+// console.log(sumNumIndex(testArray2)) // result should be [5, 5, 5, 5, 5]
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,6 @@
+var arr3 = [6, "No dice", 12, 14, "No dice", "No dice", "No dice"];
+for(var i = 0; i <= arr3.length; i++){
+    if(arr3[i] > 5){
+        console.log(arr3[i]);
+    }
+}
