828a17453dbd53c29b98aee2d382dd8e06c584b5
eranson4 <edwardranson87@gmail.com>
2023-05-07 09:34:18 -0700

    codechallengeProblemSolving

---
+++ C_challengeProbSolving.js
@@ -0,0 +1,25 @@
+/*                     User Story 
+A computer application needs to be able to identify if a written paragraph
+is written in First, Second, or Third Person
+*/
+
+/* 1. One strategy would be to pick out key words for First,Second, or Third person.
+First person key words: I, we, ours
+
+Second person key words: You, Yours, Yourselves
+
+Third person key words: Him, her, they, referencing themselves
+*/
+
+/* 2. pseudocode;
+If the paragraph has corresponding key words to First, Second, or Third person,
+return First, Second, or Third person in reltion to key words used in the
+paragraph.
+*/
+
+/* 3. 
+The hardest part about problem solving is not having the experience or 
+gained knowledge at the moment to fully asses the problem and access the
+necessary cognitive tools to solve the problem presented. Or getting lost 
+trying to find the result, due to said current inexperience.
+*/
